ddac353c7584d5c8e2cd02f4ccf65048d825bcb0
gabrielsalesls <salesgabriel311@gmail.com>
2026-06-18 19:43:08 -0300

    feat: scaffold bankcore Spring Boot base project

---
+++ bankcore/src/main/java/dev/gabrielsales/bankcore/BankcoreApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielsales.bankcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankcoreApplication.class, args);
+	}
+
+}
---
+++ bankcore/src/test/java/dev/gabrielsales/bankcore/BankcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielsales.bankcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
