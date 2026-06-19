0acf452824253fc9a2cd3eda73f978d3486b12ce
gabrielsalesls <salesgabriel311@gmail.com>
2026-06-18 21:06:06 -0300

    feat: add UserService with email uniqueness check

---
+++ bankcore/src/main/java/dev/gabrielsales/bankcore/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package dev.gabrielsales.bankcore.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String email) {
+        super("Email already exists: " + email);
+    }
+}
---
+++ bankcore/src/main/java/dev/gabrielsales/bankcore/service/UserService.java
@@ -0,0 +1,23 @@
+package dev.gabrielsales.bankcore.service;
+
+import dev.gabrielsales.bankcore.domain.entity.User;
+import dev.gabrielsales.bankcore.exception.EmailAlreadyExistsException;
+import dev.gabrielsales.bankcore.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(String name, String email, String password) {
+        if (userRepository.existsByEmail(email)) {
+            throw new EmailAlreadyExistsException(email);
+        }
+
+        User user = new User(name, email, password);
+        return userRepository.save(user);
+    }
+}
